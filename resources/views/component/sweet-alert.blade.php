3444db62c7809e4feffa0bf98541b0aa56bfd44d
creador <angelo.jalp@gmail.com>
2024-09-12 15:58:19 -0400

    detalles

---
+++ resources/views/component/sweet-alert.blade.php
@@ -0,0 +1,20 @@
+<div x-data="{ open: false }" x-show="open"
+    @sweet-alert.windows="
+    cont Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    }).fire({
+        icon: event.detail.icon,
+        title: event.detail.title,
+    })
+    ">
+
+
+</div>
